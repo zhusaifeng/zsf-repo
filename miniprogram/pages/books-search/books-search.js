5dbc61671c17883b9ce864f86d53d1d335434385
zhusaifeng <2024274693@qq.com>
2022-06-24 17:43:42 +0800

    解决书籍搜索bug

--- miniprogram/pages/books-search/books-search.js
+++ miniprogram/pages/books-search/books-search.js
@@ -27,7 +27,7 @@ Page({
     bookDetailBtn: function (event) {
         var bookId = event.currentTarget.id;
         wx.navigateTo({
-        url: '/pages/book-detail/book-detail?scanCode=0&id=' + bookId
+        url: '/pages/book-detail/book-detail?scanCode=0&bookId=' + bookId
         })
     },
 
